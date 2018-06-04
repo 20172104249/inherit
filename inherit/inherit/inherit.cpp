5abad7e8ad7907fa15d4248026e85cc2dba90e66
20172104249 <2980796841@qq.com>
2018-06-04 16:09:20 +0800

    输出如何运行函数
    mydate函数的运行顺序
    析构函数的运行顺序

--- inherit/inherit/inherit.cpp
+++ inherit/inherit/inherit.cpp
@@ -40,41 +40,6 @@ void CDate::display()
 CDate::~CDate(void)
 {
 	cout << "基类析构函数" << year << "年" << month << "月" << day << "日" << endl;
-}class CDate
-{
-protected:
-	int year;
-	int month;
-	int day;
-public:
-
-	CDate();
-	CDate(int ty, int tm, int td);
-	void display();
-	~CDate();
-};
-
-CDate::CDate()
-{
-	year = 2017;
-	month = 12;
-	day = 1;
-	cout << "基类默认构造函数" << year << "年" << month << "月" << day << "日" << endl;
-}
-CDate::CDate(int ty, int tm, int td)
-{
-	year = ty;
-	month = tm;
-	day = td;
-	cout << "基类构造函数" << year << "年" << month << "月" << day << "日" << endl;
-}
-void CDate::display()
-{
-	cout << "基类dispaly" << year << "年" << month << "月" << day << "日" << endl;
-}
-CDate::~CDate(void)
-{
-	cout << "基类析构函数" << year << "年" << month << "月" << day << "日" << endl;
 }
 class CMydate :public CDate
 {
@@ -84,7 +49,13 @@ public:
 		year = ty;
 		month = tm;
 		day = td;
+		cout << "派生类构造函数" << year << "年" << month << "月" << day << "日" << endl;
 	}
+	~CMydate()
+	{
+		cout << "派生类析构函数" << year << "年" << month << "月" << day << "日" << endl;
+	}
+
 
 };
 
